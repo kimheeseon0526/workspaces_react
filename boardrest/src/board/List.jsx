336706b99befb3cd6d93d32aea941feb7fb2614c
kimheeseon0526 <khseon0526@naver.com>
2025-08-21 17:22:01 +0900

    axios 활용한 CRUD

--- boardrest/src/board/List.jsx
+++ boardrest/src/board/List.jsx
@@ -1,9 +1,10 @@
 import React, {useState, useEffect} from "react";
-import axios from "axios";
+import axios from "axios";  //서버에서 데이터만 가져올 때 사용하는 객체
 import {useNavigate} from "react-router-dom";
 
 function List(props) {
   const [list, setList] = useState(null);
+  const navigate = useNavigate();
 
 //List 컴포넌트가 생성될 때 한 번만 호출되도록
   useEffect(() =>{
@@ -27,7 +28,9 @@ function List(props) {
         </thead>
         <tbody>
         {Array.isArray(list?.list) && list.list.map((dto) => {
-          return <tr>
+          return <tr onClick={(res) => {
+            navigate(`/read?bno=${dto.bno}`);
+          }} className="clickpoint">
             <td>{dto.bno}</td>
             <td>{dto.title}</td>
             <td>{dto.content}</td>
@@ -35,6 +38,14 @@ function List(props) {
           </tr>
         })}
         </tbody>
+        <tfoot>
+        <tr>
+          <button type="button" className="btn btn-primary" onClick={(event) =>{
+            event.preventDefault();
+            navigate('/register');
+          }}>글쓰기</button>
+        </tr>
+        </tfoot>
       </table>
   );
 }
--- boardrest/src/board/Read.jsx
+++ boardrest/src/board/Read.jsx
@@ -1,10 +1,15 @@
 import React, {useEffect, useState} from "react";
-import {useLocation} from "react-router-dom";
+import {useLocation, useNavigate} from "react-router-dom";
 import axios from "axios";
+import dayjs from "dayjs";
 
 function Read(props) {
-  const [dto, setDto] = useState(null);
+  const [dto, setDto] = useState({
+    bno: '', title: '', content:'', writerName:'', regDate:'', modDate:''
+  });
   const location = useLocation();
+  //현재 페이지의 URL 정보(path, query string 등)를 가져오는 훅
+  const navigate = useNavigate();
 
   useEffect(() => {
     const queryParams = new URLSearchParams(location.search);
@@ -23,8 +28,77 @@ function Read(props) {
         })
   }, []);
 
+  const handleClickDelete = () => {
+    const confirmed = window.confirm("삭제ㄱ?");
+    if(!confirmed) return ;
+    axios.post(`/boardrest/remove`, dto.bno, {
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+    })
+        .then((res)=> {
+          alert(res.data +"번 글이 삭제됨");
+          //삭제 성공시 list로 이동
+          navigate('/');
+        })
+        .catch((error) => {
+          console.log("error: ", error);
+        })
+  };
+
   return (
-      <div></div>
+      <div>
+        <h1 className="my-3">Guestbook Read Page</h1>
+        <div className="form-group">
+          <label htmlFor="bno">bno</label>
+          <input type="text" className="form-control" id="bno" name="bno" placeholder="bno" value={dto.bno} readOnly/>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="title">title</label>
+          <input type="text" className="form-control" id="title" name="title" placeholder="Title" value={dto.title}
+                 readOnly/>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="content">content</label>
+          <textarea className="form-control" id="content" name="content" value={dto.content} placeholder="Content"
+                    readOnly>{dto.content}</textarea>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="writer">writer</label>
+          <input type="text" className="form-control" id="writer" name="writer" placeholder="Writer"
+                 value={dto.writerName}
+                 readOnly/>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="regDate">regDate</label>
+          <input type="text" className="form-control" id="regDate" name="regDate" placeholder="regDate"
+                 value={dayjs(dto.regDate).format('YYYY-MM-DD HH:mm')}
+                 readOnly/>
+        </div>
+
+        <div className="form-group my-4">
+          <label htmlFor="modDate">modDate</label>
+
+          <input type="text" className="form-control" id="modDate" name="modDate" placeholder="modDate"
+                 value={dto.modDate}
+                 readOnly/>
+        </div>
+        <button type="button" className="btn btn-primary"
+                onClick={(event) =>{
+                  // event.preventDefault();
+                  navigate(`/modify?bno=${dto.bno}`);
+                }}
+        >수정</button>
+        <button type="button" className="btn btn-secondary" onClick={() => {
+          navigate('/'); //root로 이동
+        }}>리스트</button>
+        <button className="btn btn-danger" onClick={handleClickDelete}>삭제</button>
+      </div>
   );
 }
+
 export default Read;
